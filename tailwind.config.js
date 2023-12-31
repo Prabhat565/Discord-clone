656bbcc60f9cc531775d36ef1f86b0e99d9f6a90
Prabhat565 <sharmaps898@gmail.com>
2023-12-31 17:17:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily: {
+    ggsans: ["ggsans"],
+    ginto: ["ginto"],
+    gintonord: ["gintonord"],
+    whitney: ["whitney"],
+  },
+};
